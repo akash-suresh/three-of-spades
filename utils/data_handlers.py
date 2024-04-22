7df8ee305595b3be02c965ed10aabff72d860738
Akash Suresh <akashsuresh36@gmail.com>
2024-04-22 09:25:48 +0000

    Moved some more functions to utils

--- utils/data_handlers.py
+++ utils/data_handlers.py
@@ -4,6 +4,7 @@ import numpy as np
 # non-player columns
 NON_PLAYER_COLUMNS = {'Bidder'}
 
+# returns raw_df containing tournament scores
 def get_tourney_data(tourney_number):
 
   df = pd.read_csv(f'tourney_data/championship_{tourney_number}.csv')
@@ -19,14 +20,16 @@ def get_tourney_data(tourney_number):
 
   return df
 
-def get_players(df):
-  players = list(filter(lambda player: player not in NON_PLAYER_COLUMNS, list(df.columns[1:])))
+# returns List of players participating using raw_df
+def get_players(raw_df):
+  players = list(filter(lambda player: player not in NON_PLAYER_COLUMNS, list(raw_df.columns[1:])))
   return players
 
-def get_game_data_as_timeseries(df):
+# returns game_data which is enhanced raw_df
+def get_game_data_as_timeseries(raw_df):
   # Load the data from the CSV file
-  game_data = df.copy(deep=True)
-  players = get_players(df)
+  game_data = raw_df.copy(deep=True)
+  players = get_players(raw_df)
 
   for player in players:
     game_data[f'CumSum_{player}'] = game_data[player].cumsum()
@@ -36,3 +39,42 @@ def get_game_data_as_timeseries(df):
     game_data[f'WinRatio_{player}'] = game_data[f'NumGamesWon_{player}']/game_data['Game ID']
 
   return game_data
+
+# Get player stats from raw_df
+def get_player_stats(raw_df):
+
+  player_stats = raw_df.melt(id_vars=['Game ID'], var_name='Player', value_name='Points')
+  player_stats['Result'] = player_stats['Points'].apply(lambda x: 'Win' if x > 0 else 'Lose')
+  # Calculate wins, losses, and win ratio
+  player_stats['Wins'] = player_stats['Result'].apply(lambda x: 1 if x == 'Win' else 0)
+  player_stats['Losses'] = player_stats['Result'].apply(lambda x: 1 if x == 'Lose' else 0)
+
+  player_stats = player_stats.groupby('Player').agg(
+    Wins=pd.NamedAgg(column='Wins', aggfunc='sum'),
+    # Losses=pd.NamedAgg(column='Losses', aggfunc='sum'),
+    TotalGames=pd.NamedAgg(column='Game ID', aggfunc='count'),
+    AvgPoints=pd.NamedAgg(column='Points', aggfunc='mean'),
+    TotalPoints=pd.NamedAgg(column='Points', aggfunc='sum'),
+  ).reset_index()
+
+  player_stats['WinRatio'] = player_stats['Wins'] / (player_stats['TotalGames'])
+  player_stats = player_stats.sort_values(by=['TotalPoints', 'Wins'], ascending = False)
+  return player_stats
+
+def get_bid_and_won_stats(raw_df):
+  players = get_players(raw_df)
+  
+  bid_and_win = {}
+  for player in players:
+    bid_and_win[player] = 0
+
+  for index, row in raw_df.iterrows():
+      x = row[1:]
+      if(x.sum()%x.max()!=0):
+        for player in players:
+          if x[player] == x.max():
+            bid_and_win[player] += 1
+
+  res = pd.DataFrame(bid_and_win.items(), columns=['Player', 'Bid and Won'])
+  res = res.sort_values(by=['Bid and Won'], ascending = False)
+  return res
