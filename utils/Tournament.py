ab6e22b08f6c212d6e147200a44fcc925c99f13f
Akash Suresh <akashsuresh36@gmail.com>
2024-06-19 19:35:07 +0000

    Fixed interesting cyclic dependency super fast; side note - moved 2 graph functions

--- utils/Tournament.py
+++ utils/Tournament.py
@@ -1,6 +1,8 @@
-from utils.constants import TournamentTypes
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from utils.constants import TournamentTypes, get_graph_path
 from utils.data_cruncher import get_championship_details
-from utils.plot_utils import plot_leaderboard_barplot_v2
 
 
 class Tournament:
@@ -8,10 +10,10 @@ class Tournament:
     tournamentType: TournamentTypes
     tournamentNumber: int
     # todo create Player class
-    rawData: object
+    rawData: pd.DataFrame
     players: list[str]
-    playerStats: object
-    gameData: object
+    playerStats: pd.DataFrame
+    gameData: pd.DataFrame
 
 
     def __init__(self, tournament_type: TournamentTypes, tournament_number: int):
@@ -26,12 +28,61 @@ class Tournament:
         self.playerStats = player_stats
         self.gameData = game_data
         
+    def num(self):
+        return self.tournamentNumber
+    
+    def typ(self) -> TournamentTypes:
+        return self.tournamentType
+    
+    def display(self):
+        return f'{self.tournamentType.display()} #{self.tournamentNumber}'
+
     def getScoreBoard(self):
         return self.playerStats
     
     def getLeaderboardBarplot(self, save_image: bool = False):
-        return plot_leaderboard_barplot_v2(self.playerStats, self.tournamentType, self.tournamentNumber, save_image)
+        plot_leaderboard_barplot(self, save_image)
     
+    def getTimeseriesPlot(self, save_image: bool = False):
+        plot_leaderboard_timeseries(self, save_image)
+
+
+def plot_leaderboard_barplot(tournament: Tournament, save_image: bool = False):
+    player_stats = tournament.playerStats
+
+    plt.figure(figsize=(7, 4))
+    plt.bar(player_stats['Player'], player_stats['TotalPoints'], color=['pink', 'lightblue', 'lightgreen','lightcoral'])
+    plt.xlabel('Player')
+    plt.ylabel('Total Points')
+    plt.title(f'Leaderboard : {tournament.display()}')
+    plt.xticks(rotation=45)
+    if save_image:
+        plt.savefig(get_graph_path(tournament.num(), 'total_points'))
+    plt.show()
+
+
+def plot_leaderboard_timeseries(tournament: Tournament, save_image=False):
+
+    # Plot the points accumulated over time
+    plt.figure(figsize=(10, 6))
+
+    game_data = tournament.gameData
+    # Calculate cumulative points over time
+    for player in tournament.players:
+        plt.plot(game_data['Game ID'], game_data[f'CumSum_{player}'], marker='.', linestyle='-', label=player)
+
+    plt.legend()
+    plt.xlabel('Game Number')
+    plt.ylabel('Points Accumulated')
+    plt.title(f'Points Accumulated Over Time : {tournament.display()}')
+    plt.grid(True)
+
+    if save_image:
+        plt.savefig(get_graph_path(tournament.num(), 'points_timeseries'))
+
+    plt.show()
+
+
 '''
 from typing import List, Union
 
